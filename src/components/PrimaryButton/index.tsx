e4118f0679622e55f16da6225509c4185c77f435
Diego Arruda <arrudadiego45@gmail.com>
2022-11-13 17:48:35 -0300

    Hover in home button

--- src/components/PrimaryButton/index.tsx
+++ src/components/PrimaryButton/index.tsx
@@ -14,7 +14,7 @@ export default function PrimaryButton({ title }:PrimaryButtonProps) {
   return (
     <button
       type="submit"
-      className="flex text-3xl p-2 pr-4 pl-8 bg-main items-center gap-6 text-text rounded-full"
+      className="flex text-3xl p-2 pr-4 pl-8 bg-main items-center gap-6 text-text rounded-full hover:bg-[#1879A1] active:bg-[#0F4F6A] ease-in duration-150"
       onClick={handleNavigate}
     >
       <h1>{title}</h1>
--- src/components/SecondaryButton/index.tsx
+++ src/components/SecondaryButton/index.tsx
@@ -6,7 +6,10 @@ interface SecondaryButtonProps{
 
 export default function SecondaryButton({ title }:SecondaryButtonProps) {
   return (
-    <button type="submit" className="flex text-3xl p-2 pr-4 pl-8 bg-secondary items-center gap-6 text-text rounded-full">
+    <button
+      type="submit"
+      className="flex text-3xl p-2 pr-4 pl-8 bg-secondary items-center gap-6 text-text rounded-full hover:bg-[#002437] active:bg-[#001723] ease-in duration-150"
+    >
       <h1>{title}</h1>
       <BsTriangleFill className="rotate-90" size={25} />
     </button>
