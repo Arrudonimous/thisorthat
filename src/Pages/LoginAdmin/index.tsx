2aa82af66f277d9cedf09be5572a4b12a5ada749
Diego Arruda <arrudadiego45@gmail.com>
2022-11-13 17:28:40 -0300

    Make login

--- src/Pages/LoginAdmin/index.tsx
+++ src/Pages/LoginAdmin/index.tsx
@@ -1,22 +1,38 @@
 import { MdMail, MdLock } from 'react-icons/md';
-// import { useContext } from 'react';
+import { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import Container from '../../components/Container/Index';
 import Input from '../../components/Input';
+import { InputContext } from '../../contexts/InputContext';
+import api from '../../services/api';
 
-// import { InputContext } from '../../contexts/InputContext';
-// import api from '../../services/api';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function LoginAdmin() {
-  // const context = useContext(InputContext);
+  const navigate = useNavigate();
+  const context = useContext(InputContext);
 
-  // async function handleLogin() {
-  //   const res = await api.post('/auth/admin-login', {
-  //     email: context.email,
-  //     passwod: context.password,
-  //   });
-  //   console.log(res);
-  // }
+  async function handleLogin() {
+    try {
+      const { data } = await api.post('/auth/admin-login', {
+        email: context.email,
+        password: context.password,
+      });
+
+      toast.success(data.message, {
+        position: 'bottom-center',
+      });
+
+      api.defaults.headers.common.Authorization = data.token;
+      setTimeout(() => navigate('/view/questions'), 2700);
+    } catch (error : any) {
+      toast.error(error.response.data.message, {
+        position: 'bottom-center',
+      });
+    }
+  }
 
   return (
     <Container title="Conecte-se como admin">
@@ -29,11 +45,12 @@ export default function LoginAdmin() {
         </Input>
         <div
           className="mt-4 bg-secondary rounded-lg hover:bg-[#002437] active:bg-[#001723] ease-in duration-150"
-          // onClick={handleLogin}
+          onClick={handleLogin}
         >
-          <Button route="/view/questions" />
+          <Button />
         </div>
       </div>
+      <ToastContainer autoClose={2000} />
     </Container>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,9 @@
-import { useNavigate } from 'react-router-dom';
-
-export default function Button({ route }: { route: string}) {
-  const navigate = useNavigate();
-  function handleNavigate() {
-    return navigate(route);
-  }
-
+export default function Button() {
   return (
     <button
       type="submit"
       className="font-bold py-3 w-full text-lg"
-      onClick={handleNavigate}
+      // onClick={handleNavigate}
     >
       Entrar
 
