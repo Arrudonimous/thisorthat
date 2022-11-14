bdc20fe3c95388fd67cf15e50ff43485edf0f770
Diego Arruda <arrudadiego45@gmail.com>
2022-11-14 12:19:11 -0300

    fix

--- src/Pages/RegisterPlayer/index.tsx
+++ src/Pages/RegisterPlayer/index.tsx
@@ -1,21 +1,24 @@
 import { MdMail, MdLock } from 'react-icons/md';
 import { BsFillPersonFill } from 'react-icons/bs';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { InputContext } from '../../contexts/InputContext';
+
 import Button from '../../components/Button';
 import Container from '../../components/Container/Index';
 import Input from '../../components/Input';
-import { InputContext } from '../../contexts/InputContext';
 import api from '../../services/api';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function RegisterPlayer() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const context = useContext(InputContext);
 
   async function handleLogin() {
+    setIsLoading(true);
     try {
       const { data } = await api.post('/auth/admin-login', {
         email: context.email,
@@ -32,6 +35,8 @@ export default function RegisterPlayer() {
       toast.error(error.response.data.message, {
         position: 'bottom-center',
       });
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -51,7 +56,7 @@ export default function RegisterPlayer() {
           className="mt-4 bg-secondary rounded-lg hover:bg-[#002437] active:bg-[#001723] ease-in duration-150"
           onClick={handleLogin}
         >
-          <Button title="Registrar" />
+          <Button title="Registrar" loading={isLoading} />
         </div>
         <div className="flex justify-center font-extrabold opacity-50 gap-1">
           <h1>Já tem uma conta? </h1>
